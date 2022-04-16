a510c9a6ccba2c01dc06f26d55234b332c0e5d1e
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-04-16 11:28:24 +0900

    add: レビュー機能とお問い合わせの処理を作成した

--- lib/screen/setting_screen/setting_screen.dart
+++ lib/screen/setting_screen/setting_screen.dart
@@ -1,41 +1,22 @@
 import 'package:book_instagram_for_firebase/screen/account_screen/edit_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:in_app_review/in_app_review.dart';
 import 'package:settings_ui/settings_ui.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final InAppReview inAppReview = InAppReview.instance;
     return Scaffold(
       appBar: AppBar(),
       body: SettingsList(
         sections: [
           SettingsSection(
-            title: const Text('Common'),
+            title: const Text('設定'),
             tiles: <SettingsTile>[
-              SettingsTile.navigation(
-                leading: const Icon(Icons.language),
-                title: const Text('言語'),
-                value: const Text('日本語'),
-                onPressed: (context) {},
-              ),
-              SettingsTile.switchTile(
-                onToggle: (value) {},
-                initialValue: true,
-                leading: const Icon(Icons.format_paint),
-                title: const Text('Enable custom theme'),
-              ),
-            ],
-          ),
-          SettingsSection(
-            title: const Text('Common'),
-            tiles: <SettingsTile>[
-              SettingsTile.navigation(
-                leading: const Icon(Icons.notifications_active),
-                title: const Text('お知らせ'),
-                onPressed: (context) {},
-              ),
               SettingsTile.navigation(
                 leading: const Icon(Icons.person),
                 title: const Text('アカウント'),
@@ -49,22 +30,38 @@ class SettingScreen extends StatelessWidget {
               SettingsTile.navigation(
                 leading: const Icon(Icons.local_police),
                 title: const Text('ライセンス'),
-                onPressed: (context) {},
-              ),
-              SettingsTile.navigation(
-                leading: const Icon(Icons.help),
-                title: const Text('ヘルプ'),
-                onPressed: (context) {},
+                onPressed: (context) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const LicensePage()),
+                  );
+                },
               ),
               SettingsTile.navigation(
                 leading: const Icon(Icons.mail),
                 title: const Text('お問い合わせ'),
-                onPressed: (context) {},
+                onPressed: (context) async {
+                  const url =
+                      'https://docs.google.com/forms/d/e/1FAIpQLScvyVmwmzc_acPaCKaM0EER7iNP7zxL0YJsGqV7saEqdT8M1g/viewform?usp=sf_link';
+                  if (await canLaunch(url)) {
+                    await launch(url);
+                  } else if (await canLaunch(url)) {
+                    // 最初のURLが開けなかった場合かつセカンドURLが有って開けた場合
+                    await launch(url);
+                  } else {
+                    // 任意のエラー処理
+                  }
+                },
               ),
               SettingsTile.navigation(
                 leading: const Icon(Icons.star),
                 title: const Text('レビューする'),
-                onPressed: (context) {},
+                onPressed: (context) async {
+                  if (await inAppReview.isAvailable()) {
+                    inAppReview.requestReview();
+                  }
+                },
               ),
             ],
           ),
